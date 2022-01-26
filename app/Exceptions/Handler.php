2217c9392985c9346421a30635270de2a41cf76e
Fernando Aponte <fernandoaponte90@gmail.com>
2022-01-25 23:44:21 -0400

    Update handler exception to API access

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -34,8 +36,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Exception $e, $request) {
+            return response()->json(["status" => "Unauthorized access", "message" => "You are not allowed to access the API."], 401);
         });
     }
 }
